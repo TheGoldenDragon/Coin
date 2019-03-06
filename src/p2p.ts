e58e0508a3d1ce1b96772d31f46d491a4896db4e
Ritseart Oord <ritseart@hotmail.nl>
2019-03-06 14:56:47 +0100

    - Changed backup plan

--- src/p2p.ts
+++ src/p2p.ts
@@ -89,7 +89,7 @@ const closeConnection = (myWs: WebSocket, trigger: string) => {
         console.log('Connection '+ trigger +' with peer: ' + myWs.url);        
         sockets.splice(sockets.indexOf(myWs), 1);
         peers.splice(peers.indexOf(myWs.url), 1);
-        if(server.clients.size == 0 && peers.length == 0){
+        if(peers.length == 0){
             startupPeerList.forEach(address => {
                 console.log('Trying to connect node to: ' + address);
                 connectToPeer(address);
@@ -255,7 +255,7 @@ const connectToPeer = (newPeer: string): void => {
    
     ws.on('error', () => {
         console.log('Failed to connect to: ' + newPeer);
-        if(server.clients.size == 0 && peers.length == 0){
+        if(peers.length == 0){
             startupPeerList.forEach(address => {
                 console.log('Trying to connect node to: ' + address);
                 connectToPeer(address);
